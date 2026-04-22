dc2f41a7186dbc5e6f12605248b1056e939b9957
藤吉建介 <f2210563@gl.cc.uec.ac.jp>
2026-04-23 03:44:13 +0900

    feat: set up prisma and basic todo api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('todos')
+  getTodos() {
+    return this.appService.getTodos();
+  }
+
+  @Post('todos')
+  createTodo() {
+    return this.appService.createTodo();
   }
 }
